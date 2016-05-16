f82a0269b9b2e97441ee4d99af7fe257249dd4b6
Matt Black <matthewcoggin@gmail.com>
2016-05-15 23:42:57 -0400

    Finalize countdown tests and add page title

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -6,10 +6,6 @@ var Controls = React.createClass({
     onStatusChange: React.PropTypes.func.isRequired
   },
 
-  componentWillReceiveProps: function (newProps) {
-    console.log('componentWillReceiveProps', newProps.countdownStatus);
-  },
-
   onStatusChange: function (newStatus) {
     return () => {
       this.props.onStatusChange(newStatus);
